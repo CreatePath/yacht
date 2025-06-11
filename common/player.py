bb2ffa7f05c12578b0f587357a7d0d56c4562d72
CreatePath <hary0427@naver.com>
2025-06-11 20:45:38 +0900

    add: RandomPlayer 추가 - 주사위 조합 및 점수 카테고리 선택이 무작위. - 주사위 조합 선택: 주사위 갯수만큼 0 ~ 1 사이의 난수를 생성한 후, 0.5   이상이면 채택 - 점수 카테고리 선택: 선택 가능한 카테고리 중 1개 무작위 선택

--- common/player.py
+++ common/player.py
@@ -1,3 +1,6 @@
+import random
+import itertools
+
 from enums.score_categories import ScoreCategories
 from common.scoreboard import ScoreBoard
 
@@ -33,3 +36,23 @@ class Player:
     def showScoreBoard(self):
         print("--- {}의 점수판 ---".format(self.name))
         print(self.scoreBoard)
+
+
+class RandomPlayer(Player):
+    def __init__(self, scoreBoard: ScoreBoard, name: str):
+        super().__init__(scoreBoard, name)
+    
+    def selectDice(self, remainChances, dices):
+        randResult = [1 if random.random() >= 0.5 else 0 for _ in range(len(dices))]
+        idx = [i for i in range(1, 5) if randResult[i-1] == 1]
+        return [dices[i] for i in idx]
+
+    def selectScoreCategory(self):
+        candidates = [ category for category in self.scoreBoard.allocTable \
+                      if not self.scoreBoard.allocTable[category] and category != ScoreCategories.BONUS ]
+
+        if len(candidates) == 1:
+            return candidates[0]
+
+        randResult = random.randint(0, len(candidates)-1)
+        return candidates[randResult]
--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from common.yacht import Yacht
 from config import GameConfig
 from enums.score_categories import ScoreCategories
 from common.scoreboard import ScoreBoard
-from common.player import Player
+from common.player import Player, RandomPlayer
 from common.dealer import Dealer
 from common.bonus import Bonus
 from common.dice import Dice
 from common import scoreCalculator
 
+def generatePlayer(playerCls: Player, subCategories: list[ScoreCategories], name):
+    scoreBoard = ScoreBoard(ScoreCategories, subCategories)
+    player = playerCls(scoreBoard, name)
+    return player
+
 def main(args):
     GameConfig.NUM_PLAYERS = args.num_human + args.num_rand_agent
+    print(GameConfig.NUM_PLAYERS)
     subCategories = [ScoreCategories.ACES,
                      ScoreCategories.DEUCES,
                      ScoreCategories.THREES,
@@ -20,10 +26,12 @@ def main(args):
                      ScoreCategories.SIXES,]
 
     players = []
-    for i in range(1, GameConfig.NUM_PLAYERS+1):
-        scoreBoard = ScoreBoard(ScoreCategories, subCategories)
-        name = "P{}".format(i)
-        player = Player(scoreBoard, name)
+    for i in range(args.num_human):
+        player = generatePlayer(Player, subCategories, f"Human_{i+1}")
+        players.append(player)
+
+    for i in range(args.num_rand_agent):
+        player = generatePlayer(RandomPlayer, subCategories, f"Random_{i+1}")
         players.append(player)
 
     dealer = Dealer()
@@ -48,8 +56,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-u", "--num_human", dest="num_human", type=int, action="store", default=2)
-    parser.add_argument("-r", "--num_rand_agent", dest="num_rand_agent", type=int, action="store", default=0)
+    parser.add_argument("-u", "--num_human", dest="num_human", type=int, action="store", default=0)
+    parser.add_argument("-r", "--num_rand_agent", dest="num_rand_agent", type=int, action="store", default=2)
     args = parser.parse_args()
 
     main(args)
